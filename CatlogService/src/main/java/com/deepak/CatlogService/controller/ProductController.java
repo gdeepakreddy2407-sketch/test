cc26c0b043b7f76f31665caa72503c57c2be1f61
deepak <deepakreddy2019@gmail.com>
2025-11-30 13:06:48 +0530

    changes to controller

--- CatlogService/src/main/java/com/deepak/CatlogService/controller/ProductController.java
+++ CatlogService/src/main/java/com/deepak/CatlogService/controller/ProductController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class ProductController {
     @Autowired
     public ProductService productService;
